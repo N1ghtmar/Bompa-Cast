dda5a6ab60ff39e941466ec1d6896f3a18a68720
Bruno Luiz Bender Loss <61715805+N1ghtmar@users.noreply.github.com>
2023-07-09 16:26:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -25,6 +25,39 @@ for (var i=0;i<elems.length;i+=1){
 
 var clicou = false
 var dropdown_btn = document.getElementsByName("btn1");
+var sobre = document.getElementById("sobre_text");
+var integ = document.getElementById("int_text");
+var contato = document.getElementById("contato_text");
+
 dropdown_btn[0].addEventListener("click",function(event){if(clicou==false){mostrar("block");clicou=true;}else{clicou=false;mostrar("none");}},false);
-document.getElementsByName("sobre")[0].addEventListener("click",function(event){document.getElementsByClassName("capa")[0].style.display="none"},false);
-document.getElementsByName("home")[0].addEventListener("click",function(event){document.getElementsByClassName("capa")[0].style.display="flex"},false);
+document.getElementsByName("sobre")[0].addEventListener("click",function(event){document.getElementsByClassName("capa")[0].style.display="none";
+sobre.style.display="block";
+integ.style.display="none";
+contato.style.display="none";
+clicou=false;
+mostrar("none");
+},false);
+
+document.getElementsByName("home")[0].addEventListener("click",function(event){document.getElementsByClassName("capa")[0].style.display="flex";
+sobre.style.display="none";
+integ.style.display="none";
+contato.style.display="none";
+clicou=false;
+mostrar("none");
+},false);
+
+document.getElementsByName("integrantes")[0].addEventListener("click",function(event){document.getElementsByClassName("capa")[0].style.display="none";
+sobre.style.display="none";
+integ.style.display="block";
+contato.style.display="none";
+clicou=false;
+mostrar("none");
+},false);
+
+document.getElementsByName("contato")[0].addEventListener("click",function(event){document.getElementsByClassName("capa")[0].style.display="none";
+sobre.style.display="none";
+integ.style.display="none";
+contato.style.display="block";
+clicou=false;
+mostrar("none");
+},false);
